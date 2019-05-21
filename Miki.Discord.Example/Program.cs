abb99db4d6cfde11f5bfa52c042af6d9d10306c0
Veld <velddev@gmail.com>
2019-05-22 00:47:30 +0200

    Update example

--- Miki.Discord.Example/Program.cs
+++ Miki.Discord.Example/Program.cs
@@ -2,7 +2,7 @@
 using Miki.Cache.InMemory;
 using Miki.Discord.Caching.Stages;
 using Miki.Discord.Common;
-using Miki.Discord.Gateway.Centralized;
+using Miki.Discord.Gateway;
 using Miki.Discord.Rest;
 using Miki.Logging;
 using Miki.Net.WebSockets;
@@ -12,31 +12,33 @@ using System.Threading.Tasks;
 
 namespace Miki.Discord.Example
 {
-	// This is a test app for Miki.Discord's current interfacing.
-	// The goal of this app is to explain how the system works and why these steps are neccesary.
-	// If you're new to bot programming, this might not be the most suitable API for you, but it does promise
-	// Scaling and control over certain aspects whenever needed.
-	// For questions join the Miki Stack discord (link in README.md) or tweet me @velddev
-	class Program
+    // This is a test app for Miki.Discord's current interfacing.
+    // The goal of this app is to explain how the system works and why these steps are neccesary.
+    // If you're new to bot programming, this might not be the most suitable API for you, but it does promise
+    // Scaling and control over certain aspects whenever needed.
+    // For questions join the Miki Stack discord (link in README.md) or tweet me @velddev
+    class Program
 	{
 		// Your discord token should be placed here if you want to run the application.
-		const string Token = "TOKEN_HERE";
+		const string Token = "token-here";
 
-		// Miki.Discord is pure-async, therefore; we would like to start in an async Task.
+        // Log level required to filter out logging. Useful for when you do not want all verbose information.
+        const LogLevel logLevel = LogLevel.Debug;
+		
+        // Miki.Discord is pure-async, therefore; we would like to start in an async Task.
 		static void Main(string[] args)
-			=> new Program().MainAsync().GetAwaiter().GetResult();
-
-
-		async Task MainAsync()
+			=> MainAsync(args).GetAwaiter().GetResult();
+		static async Task MainAsync(string[] args)
 		{
-			// Enables library wide logging for all Miki libraries. Consider using this logging for debugging or general information.
-			Log.OnLog += (msg, level) =>
-			{
-				if (level >= LogLevel.Information)
-				{
-					Console.WriteLine(msg);
-				}
-			};
+            // Enables library wide logging for all Miki libraries. Consider using this logging for debugging or general information.
+            new LogBuilder().SetLogHeader(x => $"[{DateTime.UtcNow.ToLongTimeString()}]")
+                .AddLogEvent((msg, level) =>
+            {
+                if (level >= logLevel)
+                {
+                    Console.WriteLine($"{msg}");
+                }
+            }).Apply();
 
 			// A cache client is needed to store ratelimits and entities.
 			// This is an in-memory cache, and will be used as a local storage repository.
@@ -45,16 +47,16 @@ namespace Miki.Discord.Example
 			);
 
 			// Discord REST API implementation gets set up.
-			DiscordApiClient api = new DiscordApiClient(Token, cache);
+			IApiClient api = new DiscordApiClient(Token, cache);
 
 			// Discord direct gateway implementation. 
-			IGateway gateway = new CentralizedGatewayShard(new GatewayConfiguration
+			IGateway gateway = new GatewayCluster(new GatewayProperties
 			{
-				ApiClient = api,
 				ShardCount = 1,
 				ShardId = 0,
 				Token = Token,
-				WebSocketClient = new BasicWebSocketClient()
+                Compressed = true,
+				WebSocketClientFactory = () => new BasicWebSocketClient()
 			});
 
 			// This function adds additional utility, caching systems and more.
